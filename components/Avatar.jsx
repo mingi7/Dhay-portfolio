8a47462c93cfde40a56d7959d4ec49f0a93bc011
mingi7 <148896634+mingi7@users.noreply.github.com>
2026-06-05 01:57:09 +0300

    صورة

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -4,8 +4,8 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none pointer-events-none select-none">
       <Image
-        src="/avatar.png"
-        alt="avatar"
+        src="/dodi.png"
+        alt ="dodi"
         width={737}
         height={678}
         className="translate-z-0 w-full h-full"
