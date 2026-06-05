fbf6b573d1fc8332674ad83bb23cca7759b22b5c
mingi7 <148896634+mingi7@users.noreply.github.com>
2026-06-06 01:08:17 +0300

    نجرب

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -97,18 +97,6 @@ const About = () => {
   return (
     <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
 
-      {/* avatar img */}
-      <motion.div
-        variants={fadeIn("right", 0.2)}
-        initial="hidden"
-        animate="show"
-        exit="hidden"
-        className="hidden xl:flex absolute bottom-0 -left-[370px]"
-      >
-        <Avatar />
-      </motion.div>
-
-      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         {/* text */}
         <div className="flex-1 flex flex-col justify-center">
           <motion.h2
