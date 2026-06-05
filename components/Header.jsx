166b9479c79c1d04310556d93a6c2dc6af4b769d
mingi7 <148896634+mingi7@users.noreply.github.com>
2026-06-06 02:47:03 +0300

    4

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
           {/* logo */}
           <Link href="/">
             <Image
-              src="/public/file.svg"
+              src="/file.svg"
               alt="logo"
               width={220}
               height={48}
