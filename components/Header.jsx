30c1a7890d5787f2e3a3a380c466becae1aecbec
mingi7 <148896634+mingi7@users.noreply.github.com>
2026-06-06 23:48:09 +0300

    الليل والنهار

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,32 @@
+import { useState } from "react";
 import Link from "next/link";
 import Socials from "../components/Socials";
 
 const Header = () => {
+  const [isDark, setIsDark] = useState(true);
+
+  // دالة تغيير الخلفية
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+    document.body.className = isDark ? "light-mode" : "dark-mode";
+  };
+
   return (
-    <header className="absolute z-30 w-full items-center px-16 xl:px-0 xl:h-[90px]">
+    <header className={`${isDark ? "text-white" : "text-black"} absolute z-30 w-full items-center px-16 xl:px-0 xl:h-[90px]`}>
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
-          {/* logo */}
+          
           <Link href="/">
-            <div className="text-white text-3xl font-bold cursor-pointer">
+            <div className="text-3xl font-bold cursor-pointer">
               Dhay Alzaidi
             </div>
           </Link>
 
-          {/* socials */}
+          {/* زر التبديل */}
+          <button onClick={toggleTheme} className="text-2xl">
+            {isDark ? "☀️" : "🌙"}
+          </button>
+
           <Socials />
         </div>
       </div>
